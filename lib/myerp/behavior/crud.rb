15e88d73234c56bf17b4906b1df1261cb62fd14b
dbaq <didier.baquier@myerp.com>
2014-03-05 18:30:53 -0800

    add the support for bulk creation/modification

--- lib/myerp/behavior/crud.rb
+++ lib/myerp/behavior/crud.rb
@@ -23,6 +23,12 @@ module MyERP
         MyERP::Response.new(resp.headers, api_model.parse(resp.parsed_response).first)
       end
 
+      def bulkSave(models)
+        raise "array required" unless models.is_a?(Array)
+        resp = request(:put, credentials, "#{api_model.api_path}", :body => models.to_json)
+        MyERP::Response.new(resp.headers, api_model.parse(resp.parsed_response))
+      end
+
       def delete(model)
         if model.is_a?(Array)
           resp = request(:delete, credentials, "#{api_model.api_path}", :body => model.to_json)
