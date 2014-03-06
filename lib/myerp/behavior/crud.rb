f008e969b22fe0f4aaa8ff6767a8258c884daa91
dbaq <didier.baquier@myerp.com>
2014-03-05 17:08:11 -0800

    use a single method save() to create and update

--- lib/myerp/behavior/crud.rb
+++ lib/myerp/behavior/crud.rb
@@ -13,23 +13,20 @@ module MyERP
         api_model.parse(response.parsed_response).first
       end
 
-      def create(model)
+      def save(model)
         model = api_model.wrap(model)
-        response = request(:post, credentials, "#{api_model.api_path}", :body => model.to_json)
-        id = response.headers["location"].match(/\/.*\/(\d+)/)[1]
-        find(id)
-      end
-
-      def update(model)
-        model = api_model.wrap(model)
-        request(:put, credentials, "#{api_model.api_path}/#{model.to_i}", :body => model.to_json)
-        find(model.id)
+        if model.new?
+          response = request(:post, credentials, "#{api_model.api_path}", :body => model.to_json)
+        else
+          response = request(:put, credentials, "#{api_model.api_path}/#{model.to_i}", :body => model.to_json)
+        end
+        api_model.parse(response.parsed_response).first
       end
 
       def delete(model)
-	raise "model unsaved" unless !model.new?
-	response = request(:delete, credentials, "#{api_model.api_path}/#{model.to_i}")
-	api_model.parse(response.parsed_response).first
+        raise "model unsaved" unless !model.new?
+        response = request(:delete, credentials, "#{api_model.api_path}/#{model.to_i}")
+        api_model.parse(response.parsed_response).first
       end
     end
   end
